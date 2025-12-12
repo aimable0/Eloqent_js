f6a6c4ece67c7509371d337fb1b21a17b289cae9
aimable0 <aimablenkurikiyimana1@gmail.com>
2025-12-12 08:58:51 -0800

    refining range function with an extra feature

--- chapter_4/range.js
+++ chapter_4/range.js
@@ -1,20 +1,28 @@
-function range(start, end) {
+function range(start, end, step=(start < end)?1:-1) {
     let list = [];
-    for (let i = start; i <= end; i++) {
+    
+    // version 1.
+    // if (step < 0) {
+    //     for (let i = start; i >= end; i -= -(step)) list.push(i);
+    // } else {
+    //     for (let i = start; i <= end; i += step) list.push(i);
+    // }
+
+    // version 2.
+    for (let i = start; (start < end) ? i <= end : i >= end; i += step)
         list.push(i);
-    }
     return list;
 }
 
-console.log(range(1, 10)); // -> [1, ..., 10]
+console.log(range(5, 10));
 
-function sum(numbers) {
-    let sum = 0;
-    for (let number of numbers) sum += number;
-    return sum;
-}
+// function sum(numbers) {
+//     let sum = 0;
+//     for (let number of numbers) sum += number;
+//     return sum;
+// }
 
-console.log(sum(range(1, 10))) // -> 55
+// console.log(sum(range(1, 10))) // -> 55
 
 // trying out something
 // console.log();
@@ -24,3 +32,6 @@ console.log(sum(range(1, 10))) // -> 55
 //     }
 // }
 // print(range(1, 10));
+
+// BONUS ASSIGNMENT
+
